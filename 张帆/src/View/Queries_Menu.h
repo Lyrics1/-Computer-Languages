ef01f613c22a7167edeb71fde16658144c7b6c44
Lyrics1 <lyricsfan@aliyun.com>
2017-09-30 07:56:18 +0800

    Remove the now ignored directory some-directory

--- 张帆/src/View/Queries_Menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUERIES_MENU_H_
-#define QUERIES_MENU_H_
-#include "../Common/list.h"
-#include "../Service/Play.h"
-
-void Queries_Menu(void);
-
-
-#endif /* QUERIES_MENU_H_ */
--- 张帆/src/View/Query_Play_UI.h
+++
@@ -1,8 +0,0 @@
-#ifndef QUERY_PLAY_UI_H
-#define QUERY_PLAY_UI_H
-
-#include "../Service/Play.h"
-
-void DisplayQueryPlay(void);
-
-#endif
